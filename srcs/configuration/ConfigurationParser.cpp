69b41dec17a3ea1d3b2d4df31c559548f6662b59
UnitedIngvar <ingvarpetrova@gmail.com>
2022-07-10 20:54:56 +0300

    [WSV-3] added some excetptions, created the initial config parser hpp file

--- srcs/configuration/ConfigurationParser.cpp
+++ srcs/configuration/ConfigurationParser.cpp
@@ -17,12 +17,14 @@ Configuration ConfigurationParser::parseConfig(std::string pathToFile)
 {
 	File file(pathToFile);
 	std::string fileContents;
+	Configuration Configuration;
 
 	FileOperationResult readResult = file.readFile(fileContents);
 	if (readResult != Success)
 	{
 		throw new FileReadException(pathToFile);
 	}
+
 
 }
 
@@ -32,4 +34,9 @@ const char	*ConfigurationParser::FileReadException::what() const throw()
 		"error while reading a file. File path: " + _filePath + "\n";
 
 	return errorMessage.c_str();
+}
+
+const char	*ConfigurationParser::ConfigurationFormatError::what() const throw()
+{
+	return "Configuration file has invalid format";
 }
