63eb3ec922c9b4cb8903d98c9042d6e5c6d9e0fa
Artyom Zhdanov <zhdanov.artem4@corp.wildberries.ru>
2022-07-31 18:45:55 +0300

    save commit before local merge

---
+++ srcs/configuration/ConfigurationParserFuncs.cpp
@@ -0,0 +1,55 @@
+#include "ConfigurationParser.hpp"
+
+void ConfigurationParser::parseListen(const std::string &line)
+{
+
+}
+
+void ConfigurationParser::parseServerName(const std::string &line)
+{
+
+}
+
+void ConfigurationParser::parseRoot(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseErrorPagesMap(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseRedirection(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseMaxBodySize(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseLocation(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseMethods(const std::string &line)
+{
+
+}
+void ConfigurationParser::parseDirectory(const std::string &line)
+{
+
+}
+
+void ConfigurationParser::parseIndexFile(const std::string &line)
+{
+
+}
+
+void ConfigurationParser::parseCgiExtension(const std::string &line)
+{
+
+}
+
+void ConfigurationParser::parseDirListOn(const std::string &line)
+{
+
+}
