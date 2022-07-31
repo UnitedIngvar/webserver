37bb1dc1a84fa4d385a435949d158f08d2e1fbf6
UnitedIngvar <ingvarpetrova@gmail.com>
2022-07-31 17:46:15 +0300

    [WSV-5] corrected some bugs, added more tests

--- srcs/factories/RequestFactory.cpp
+++ srcs/factories/RequestFactory.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 
 std::string		RequestFactory::getStringTillChars(
-	std::string const &request, int &offset, std::string const &chars)
+	std::string const &request, size_t &offset, std::string const &chars)
 {
-	int symbolPosition = request.find(chars, offset);
+	size_t symbolPosition = request.find(chars, offset);
+
+	if (symbolPosition == std::string::npos)
+	{
+		return "";
+	}
+
 	std::string stringTillSymbol = request.substr(offset, symbolPosition - offset);
 
 	offset = symbolPosition + chars.length();
@@ -12,6 +18,28 @@ std::string		RequestFactory::getStringTillChars(
 	return stringTillSymbol;
 }
 
+bool				RequestFactory::checkPrerequisits(std::string const &request)
+{
+	size_t offset = 0;
+
+	int crlfCount = 0;
+	const std::string crlf = "\r\n";
+	for (size_t i = 0; i < request.length(); i++)
+	{
+		size_t crlfPosition = request.find(crlf, offset);
+		if (crlfPosition == std::string::npos)
+		{
+			break;
+		}
+
+		offset = crlfPosition + crlf.length();
+		crlfCount++;
+	}
+
+	return crlfCount >= 3;
+}
+
+// todo: временное решение. В будущем нужно придумать что-то получше.
 RequestMethod	RequestFactory::mapRequestMethod(std::string const &methodString)
 {
 	if (methodString.compare("GET") == 0)
@@ -85,9 +113,9 @@ Url				*RequestFactory::parseUrl(std::string const &url)
 	}
 	else
 	{
-		int urlOffset = 0;
-		path = getStringTillChars(url, urlOffset, "?");
-		query = url.substr(urlOffset);
+		size_t urlOffset = 0;
+		path = getStringTillChars(decodedUrl, urlOffset, "?");
+		query = decodedUrl.substr(urlOffset);
 	}
 
 	return new Url(
@@ -96,9 +124,48 @@ Url				*RequestFactory::parseUrl(std::string const &url)
 		query);
 }
 
+std::map<std::string, std::string>	RequestFactory::parseHeaders(std::string const &request, size_t &offset)
+{
+	std::map<std::string, std::string> headers;
+	size_t lengthTillCRLF = offset - request.find("\r\n", offset);
+	if (lengthTillCRLF != 0)
+	{
+		do
+		{
+			std::string headerName = getStringTillChars(request, offset, ":");
+			offset++;
+			std::string headerValue = getStringTillChars(request, offset, "\r\n");
+			headers.insert(std::pair<std::string, std::string>(headerName, headerValue));
+		} while (request.compare(offset, 2, "\r\n") != 0);
+	}
+	else
+	{
+		offset += 2;
+	}
+
+	return headers;
+}
+
+Request				*RequestFactory::createInvalidRequestModel()
+{
+	Url *urlModel = new Url("", "", "");
+	return new Request(
+		INVALID,
+		std::map<std::string, std::string>(),
+		*urlModel,
+		"",
+		"");
+}
+
 Request			*RequestFactory::create(std::string const &request)
 {
-	int offset = 0;
+	size_t offset = 0;
+
+	if (checkPrerequisits(request) == false)
+	{
+		Request *request = createInvalidRequestModel();
+		return request;
+	}
 
 	/* Pasrse the request line */
 	RequestMethod	method;
@@ -109,19 +176,10 @@ Request			*RequestFactory::create(std::string const &request)
 	method = mapRequestMethod(methodString);
 	url = getStringTillChars(request, offset, " ");
 	httpVersion = getStringTillChars(request, offset, "\r\n");
-
-	/* Parse the url*/
 	Url *urlModel = parseUrl(url);
 
 	/* Parse header fields */
-	std::map<std::string, std::string> headers;
-	do
-	{
-		std::string headerName = getStringTillChars(request, offset, ":");
-		offset++;
-		std::string headerValue = getStringTillChars(request, offset, "\r\n");
-		headers.insert(std::pair<std::string, std::string>(headerName, headerValue));
-	} while (request.compare(offset, 2, "\r\n") != 0);
+	std::map<std::string, std::string> headers = parseHeaders(request, offset);
 
 	/* Parse the body*/
 	offset += 2;
--- srcs/factories/includes/RequestFactory.hpp
+++ srcs/factories/includes/RequestFactory.hpp
@@ -8,10 +8,13 @@
 class RequestFactory
 {
 private:
-	RequestMethod		mapRequestMethod(std::string const &methodString);
-	std::string			getStringTillChars(std::string const &request, int &offset, std::string const &chars);
-	std::string			decodeUrl(std::string const &url);
-	Url					*parseUrl(std::string const &url);
+	bool								checkPrerequisits(std::string const &request);
+	Request								*createInvalidRequestModel();
+	RequestMethod						mapRequestMethod(std::string const &methodString);
+	std::string							getStringTillChars(std::string const &request, size_t &offset, std::string const &chars);
+	std::string							decodeUrl(std::string const &url);
+	Url									*parseUrl(std::string const &url);
+	std::map<std::string, std::string>	parseHeaders(std::string const &request, size_t &offset);
 
 public:
 	Request				*create(std::string const &requestString);
